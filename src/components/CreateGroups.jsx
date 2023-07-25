cc2c02ed655cb7b4338ea7f891900a2b78211e86
hottouk <hottouk@naver.com>
2023-07-25 21:13:28 +0900

    저장버튼 생성

--- src/components/CreateGroups.jsx
+++ src/components/CreateGroups.jsx
@@ -10,6 +10,29 @@ const CreateGroups = () => {
   return (
     <div>
       <h1>Dutch Pay</h1>
+      <Container>
+        <Form noValidate>
+          <Row>
+            <h2>먼저, 더치페이 할 그룹의 이름을 정해볼까?</h2>
+          </Row>
+          <Row>
+            <Form.Group controlId='validationGroupName'>
+              <Form.Control
+                type='text' 
+                required
+                placeholder='2022 제주도 여행' />
+              <Form.Control.Feedback type='invalid'>
+                그룹 이름을 입력해 주세요.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Button type='submit'>
+              저장
+            </Button>
+          </Row>
+        </Form>
+      </Container>
       <CenteredOverlayForm />
     </div>
 
